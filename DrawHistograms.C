34c741a537f38d2e05019edc15f1088f1484b87e
Antony Varlamov <varlamov.AM@phystech.edu>
2019-11-26 18:03:39 +0300

    New version:
    print all histograms

--- DrawHistograms.C
+++ DrawHistograms.C
@@ -273,100 +273,100 @@ DrawHistograms()
   hChiC1_y_cndtn_2     ->SetLineWidth(2);
 
 
-  //TCanvas *c1 = new TCanvas("c1","hChiC2_pt_all",0,0,800,600);
-  //hChiC2_pt_all        ->Draw();
+  TCanvas *c1 = new TCanvas("c1","hChiC2_pt_all",0,0,800,600);
+  hChiC2_pt_all        ->Draw();
   //c1->Print("hChiC2_pt_all.pdf");
 
-  //TCanvas *c2 = new TCanvas("c2","hgamma",0,0,800,600);
-  //hGamma_pt_all        ->Draw();
+  TCanvas *c2 = new TCanvas("c2","hgamma",0,0,800,600);
+  hGamma_pt_all        ->Draw();
   //c2->Print("hGamma_pt-all.pdf");
 
-  //TCanvas *c3 = new TCanvas("c3","hElectron",0,0,800,600);
-  //hElectron_pt_all     ->Draw();
+  TCanvas *c3 = new TCanvas("c3","hElectron",0,0,800,600);
+  hElectron_pt_all     ->Draw();
   // c3->Print("hElectron_pt-all.pdf");
 
-  //TCanvas *c4 = new TCanvas("c4","hPositron",0,0,800,600);
-  //hPositron_pt_all     ->Draw();
+  TCanvas *c4 = new TCanvas("c4","hPositron",0,0,800,600);
+  hPositron_pt_all     ->Draw();
   //c4->Print("hPositron_pt-all.pdf");
 
-  //TCanvas *c5 = new TCanvas("c5","hChiC2_pt_cndtn_1",0,0,800,600);
-  //hChiC2_pt_cndtn_1    ->Draw();
+  TCanvas *c5 = new TCanvas("c5","hChiC2_pt_cndtn_1",0,0,800,600);
+  hChiC2_pt_cndtn_1    ->Draw();
   //c5->Print("hChiC2_pt_cndtn_1.pdf");
 
-  //TCanvas *c6 = new TCanvas("c6","hChiC2_pt_cndtn_2",0,0,800,600);
-  //hChiC2_pt_cndtn_2    ->Draw();
+  TCanvas *c6 = new TCanvas("c6","hChiC2_pt_cndtn_2",0,0,800,600);
+  hChiC2_pt_cndtn_2    ->Draw();
   //c6->Print("hChiC2_pt_cndtn_2.pdf");
 
-  //TCanvas *c7 = new TCanvas("c7","hChiC2_y_cndtn_1",0,0,800,600);
-  //hChiC2_y_cndtn_1     ->Draw();
+  TCanvas *c7 = new TCanvas("c7","hChiC2_y_cndtn_1",0,0,800,600);
+  hChiC2_y_cndtn_1     ->Draw();
   //c7->Print("hChiC2_y_cndtn_1.pdf");
 
-  //TCanvas *c8 = new TCanvas("c8","hChiC2_y_cndtn_2",0,0,800,600);
-  //hChiC2_y_cndtn_2     ->Draw();
+  TCanvas *c8 = new TCanvas("c8","hChiC2_y_cndtn_2",0,0,800,600);
+  hChiC2_y_cndtn_2     ->Draw();
   //c8->Print("hChiC2_y_cndtn_2.pdf");
 
-  //TCanvas *c9 = new TCanvas("c9","hChiC0_pt_all",0,0,800,600);
-  //hChiC0_pt_all     ->Draw();
+  TCanvas *c9 = new TCanvas("c9","hChiC0_pt_all",0,0,800,600);
+  hChiC0_pt_all     ->Draw();
   //c9->Print("hChiC0_pt_all.pdf");
 
-  //TCanvas *c10 = new TCanvas("c10","hgamma_chic0",0,0,800,600);
-  //hGamma_chic0_pt_all        ->Draw();
+  TCanvas *c10 = new TCanvas("c10","hgamma_chic0",0,0,800,600);
+  hGamma_chic0_pt_all        ->Draw();
   //c10->Print("hGamma_pt-all.pdf");
 
-  //TCanvas *c11 = new TCanvas("c11","hElectron_chic0",0,0,800,600);
-  //hElectron_chic0_pt_all     ->Draw();
+  TCanvas *c11 = new TCanvas("c11","hElectron_chic0",0,0,800,600);
+  hElectron_chic0_pt_all     ->Draw();
   //c11->Print("hElectron_pt-all.pdf");
 
-  //TCanvas *c12 = new TCanvas("c12","hPositron_chic0",0,0,800,600);
-  //hPositron_chic0_pt_all     ->Draw();
+  TCanvas *c12 = new TCanvas("c12","hPositron_chic0",0,0,800,600);
+  hPositron_chic0_pt_all     ->Draw();
   //c12->Print("hPositron_pt-all.pdf");
 
-  //TCanvas *c13 = new TCanvas("c13","hChiC0_pt_cndtn_1",0,0,800,600);
-  //hChiC0_pt_cndtn_1    ->Draw();
+  TCanvas *c13 = new TCanvas("c13","hChiC0_pt_cndtn_1",0,0,800,600);
+  hChiC0_pt_cndtn_1    ->Draw();
   //c13->Print("hChiC2_pt_cndtn_1.pdf");
 
-  //TCanvas *c14 = new TCanvas("c14","hChiC0_pt_cndtn_2",0,0,800,600);
-  //hChiC0_pt_cndtn_2    ->Draw();
+  TCanvas *c14 = new TCanvas("c14","hChiC0_pt_cndtn_2",0,0,800,600);
+  hChiC0_pt_cndtn_2    ->Draw();
   //c14->Print("hChiC2_pt_cndtn_2.pdf");
 
-  //TCanvas *c15 = new TCanvas("c15","hChiC0_y_cndtn_1",0,0,800,600);
-  //hChiC0_y_cndtn_1     ->Draw();
+  TCanvas *c15 = new TCanvas("c15","hChiC0_y_cndtn_1",0,0,800,600);
+  hChiC0_y_cndtn_1     ->Draw();
   //c15->Print("hChiC2_y_cndtn_1.pdf");
 
-  //TCanvas *c16 = new TCanvas("c16","hChiC0_y_cndtn_2",0,0,800,600);
-  //hChiC0_y_cndtn_2     ->Draw();
+  TCanvas *c16 = new TCanvas("c16","hChiC0_y_cndtn_2",0,0,800,600);
+  hChiC0_y_cndtn_2     ->Draw();
   //c16->Print("hChiC0_y_cndtn_2.pdf");
 
-  //TCanvas *c17 = new TCanvas("c17","hChiC1_pt_all",0,0,800,600);
-  //hChiC1_pt_all     ->Draw();
+  TCanvas *c17 = new TCanvas("c17","hChiC1_pt_all",0,0,800,600);
+  hChiC1_pt_all     ->Draw();
   //c9->Print("hChiC1_pt_all.pdf");
 
-  //TCanvas *c18 = new TCanvas("c18","hgamma_chic1",0,0,800,600);
-  //hGamma_chic1_pt_all        ->Draw();
+  TCanvas *c18 = new TCanvas("c18","hgamma_chic1",0,0,800,600);
+  hGamma_chic1_pt_all        ->Draw();
   //c10->Print("hGamma_chic1_pt-all.pdf");
 
-  //TCanvas *c19 = new TCanvas("c19","hElectron_chic1",0,0,800,600);
-  //hElectron_chic1_pt_all     ->Draw();
+  TCanvas *c19 = new TCanvas("c19","hElectron_chic1",0,0,800,600);
+  hElectron_chic1_pt_all     ->Draw();
   //c11->Print("hElectron_chic1_pt-all.pdf");
 
-  //TCanvas *c20 = new TCanvas("c20","hPositron_chic1",0,0,800,600);
-  //hPositron_chic1_pt_all     ->Draw();
+  TCanvas *c20 = new TCanvas("c20","hPositron_chic1",0,0,800,600);
+  hPositron_chic1_pt_all     ->Draw();
   //c12->Print("hPositron_chic1_pt-all.pdf");
 
-  //TCanvas *c21 = new TCanvas("c21","hChiC0_pt_cndtn_1",0,0,800,600);
-  //hChiC1_pt_cndtn_1    ->Draw();
+  TCanvas *c21 = new TCanvas("c21","hChiC0_pt_cndtn_1",0,0,800,600);
+  hChiC1_pt_cndtn_1    ->Draw();
   //c13->Print("hChiC1_pt_cndtn_1.pdf");
 
-  //TCanvas *c22 = new TCanvas("c22","hChiC0_pt_cndtn_2",0,0,800,600);
-  //hChiC1_pt_cndtn_2    ->Draw();
+  TCanvas *c22 = new TCanvas("c22","hChiC0_pt_cndtn_2",0,0,800,600);
+  hChiC1_pt_cndtn_2    ->Draw();
   //c14->Print("hChiC1_pt_cndtn_2.pdf");
 
-  //TCanvas *c23 = new TCanvas("c23","hChiC0_y_cndtn_1",0,0,800,600);
-  //hChiC1_y_cndtn_1     ->Draw();
+  TCanvas *c23 = new TCanvas("c23","hChiC0_y_cndtn_1",0,0,800,600);
+  hChiC1_y_cndtn_1     ->Draw();
   //c15->Print("hChiC1_y_cndtn_1.pdf");
 
-  //TCanvas *c24 = new TCanvas("c24","hChiC0_y_cndtn_2",0,0,800,600);
-  //hChiC1_y_cndtn_2     ->Draw();
+  TCanvas *c24 = new TCanvas("c24","hChiC0_y_cndtn_2",0,0,800,600);
+  hChiC1_y_cndtn_2     ->Draw();
   //c16->Print("hChiC1_y_cndtn_2.pdf");
 
 }
