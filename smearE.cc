1589d986fe397b60f133c0f07c5d23f50da48213
Yuri Kharlov <Yuri.Kharlov@cern.ch>
2020-04-15 17:20:38 +0300

    Momentum and energy resolution are changed to up-to-date values of ALICE detectors

--- smearE.cc
+++ smearE.cc
@@ -5,7 +5,8 @@
 Double_t smearE(Double_t Etrue)
 {
   // Generate smeared photon energy from the true energy
-  const Double_t a = 0.04, b = 0.036, c = 0.01;
+  const Double_t a = 0.018, b = 0.033, c = 0.011; // Energy resolution of ALICE PHOS
+  // const Double_t a = 0.00, b = 0.000, c = 0.00; // for studies ideal resolution
   Double_t sigmaE = Etrue * sqrt(a*a/Etrue/Etrue + b*b/Etrue + c*c);
   Double_t Esmeared = gRandom->Gaus(Etrue,sigmaE);
   if (Esmeared<0) Esmeared = 0;
--- smearP.cc
+++ smearP.cc
@@ -5,7 +5,8 @@
 Double_t smearP(Double_t Ptrue)
 {
   // Generate smeared track 3-momentum from the true 3-momentum
-  const Double_t a=0.01, b=0.001;
+  const Double_t a=0.008, b=0.002; // Momentum resolution of ALICE treaking system
+  // const Double_t a=0.00, b=0.000; // for studies ideal resolution
   Double_t sigmaP = Ptrue * sqrt(a*a + b*Ptrue*b*Ptrue);
   Double_t Psmeared = gRandom->Gaus(Ptrue,sigmaP);
   if (Psmeared<0) Psmeared = 0;
